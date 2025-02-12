13e90593e0e69113453a42ac9040a8815de43283
Leonid075 <grebenukleonid075@gmail.com>
2025-02-12 11:35:23 +0300

    update Coordinates.java

--- src/main/java/ru/p3xi/labwork/Coordinates.java
+++ src/main/java/ru/p3xi/labwork/Coordinates.java
@@ -1,6 +1,45 @@
 package ru.p3xi.labwork;
 
 public class Coordinates {
-    private Integer x; //Максимальное значение поля: 970, Поле не может быть null
-    private Float y; //Максимальное значение поля: 721, Поле не может быть null
+    private Integer x; // Максимальное значение поля: 970, Поле не может быть null
+    private Float y; // Максимальное значение поля: 721, Поле не может быть null
+
+    public Coordinates(Integer x, Float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public Float getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return x.toString() + "_" + y.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        if (obj.getClass() != this.getClass())
+            return false;
+        final Coordinates discipline = (Coordinates) obj;
+        if (!discipline.getX().equals(this.x))
+            return false;
+        if (!discipline.getY().equals(this.y))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = x % 100;
+        hash += y % 100;
+        return hash;
+    }
 }
