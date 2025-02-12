d1ca240192ed2858f734e99ef1abe74f1705de1e
Leonid075 <grebenukleonid075@gmail.com>
2025-02-12 11:26:19 +0300

    update Discipline.java

--- src/main/java/ru/p3xi/labwork/Discipline.java
+++ src/main/java/ru/p3xi/labwork/Discipline.java
@@ -1,8 +1,71 @@
 package ru.p3xi.labwork;
 
 public class Discipline {
-    private String name; //Поле не может быть null, Строка не может быть пустой
+    private String name; // Поле не может быть null, Строка не может быть пустой
     private long lectureHours;
-    private Long practiceHours; //Поле не может быть null
-    private Integer labsCount; //Поле может быть null
+    private Long practiceHours; // Поле не может быть null
+    private Integer labsCount; // Поле может быть null
+
+    public Discipline(String name, Long practiceHours, Integer labsCount) {
+        this.name = name;
+        this.practiceHours = practiceHours;
+        this.labsCount = labsCount;
+        this.lectureHours = 0;
+    }
+
+    public Discipline(String name, long lectureHours, Long practiceHours, Integer labsCount) {
+        this.name = name;
+        this.practiceHours = practiceHours;
+        this.labsCount = labsCount;
+        this.lectureHours = lectureHours;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getLectureHours() {
+        return lectureHours;
+    }
+
+    public Long getPracticeHours() {
+        return practiceHours;
+    }
+
+    public Integer getLabsCount() {
+        return labsCount;
+    }
+
+    @Override
+    public String toString() {
+        return name + "_" + ((Long) lectureHours).toString() + "_" + practiceHours.toString() + "_"
+                + labsCount.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        if (obj.getClass() != this.getClass())
+            return false;
+        final Discipline discipline = (Discipline) obj;
+        if (!discipline.getName().equals(this.name))
+            return false;
+        if (!discipline.getLabsCount().equals(this.labsCount))
+            return false;
+        if (discipline.getLectureHours() != this.lectureHours)
+            return false;
+        if (!discipline.getPracticeHours().equals(this.practiceHours))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = name.hashCode();
+        hash += lectureHours % 100;
+        hash += practiceHours % 100;
+        hash += labsCount % 100;
+        return hash;
+    }
 }
