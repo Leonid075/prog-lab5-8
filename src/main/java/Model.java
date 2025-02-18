f1b6b4af7b774038d68d6d6c1b78e798d3138813
Leonid075 <grebenukleonid075@gmail.com>
2025-02-18 13:10:58 +0300

    update Model.java

--- src/main/java/Model.java
+++ src/main/java/Model.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.TreeSet;
 
 import ru.p3xi.labwork.*;
@@ -41,7 +43,7 @@ public class Model {
         Iterator<LabWork> iterator = labs.iterator();
         while (iterator.hasNext()) {
             LabWork labWork = iterator.next();
-            if (labWork.getId() == id){
+            if (labWork.getId() == id) {
                 labs.remove(labWork);
                 return;
             }
@@ -52,4 +54,54 @@ public class Model {
         labs.clear();
     }
 
+    public void removeByDiff(Difficulty diff) {
+        for (LabWork labWork : labs) {
+            if (labWork.getDifficulty().equals(diff)) {
+                labs.remove(labWork);
+            }
+        }
+    }
+
+    public void removeLower(LabWork exLabWork) {
+        while (true) {
+            LabWork labWork = labs.lower(exLabWork);
+            if (labWork == null)
+                break;
+            labs.remove(labWork);
+        }
+    }
+
+    public float averageMinimalPoint() {
+        float average = 0;
+        for (LabWork labWork : labs) {
+            average += labWork.getMinimalPoint();
+        }
+        return average / labs.size();
+    }
+
+    public boolean addIfMax(LabWork labWork) {
+        if(labs.higher(labWork) == null) {
+            labs.add(labWork);
+            return true;
+        }
+        else return false;
+    }
+
+    public boolean addIfMin(LabWork labWork) {
+        if(labs.lower(labWork) == null) {
+            labs.add(labWork);
+            return true;
+        }
+        else return false;
+    }
+
+    public List<Discipline> getDisciplines() {
+        ArrayList<Discipline> disciplines = new ArrayList<>();
+        Iterator<LabWork> iterator = labs.iterator();
+        while (iterator.hasNext()) {
+            LabWork labWork = iterator.next();
+            disciplines.add(labWork.getDiscipline());
+        }
+        return disciplines.reversed();
+    }
 }
