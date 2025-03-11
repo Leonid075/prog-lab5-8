3f75ff71c0ef3157bce54aa7255c3844cc942816
Leonid075 <grebenukleonid075@gmail.com>
2025-03-11 17:54:02 +0300

    update Main.java for test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,26 @@
+import ru.p3xi.cm.Model;
 import ru.p3xi.labwork.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            call();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void call() throws Exception {
+        Model model = new Model();
+        model.add(new LabWork("123", new Coordinates(1, 1.2f), 0.1f, Difficulty.EASY, new Discipline("321", 321, (long) 3, 2)));
+        // Coordinates cords = ;
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.findAndRegisterModules();
+    	String jsonBook = mapper.writeValueAsString(model);
+    	System.out.println(jsonBook);
     }
 }
