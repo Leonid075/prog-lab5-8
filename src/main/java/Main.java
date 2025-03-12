4d7c272e9932fce073ada65bac2ef69f053ba19d
Leonid075 <grebenukleonid075@gmail.com>
2025-03-12 12:59:54 +0300

    update Main.java for tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,13 +14,18 @@ public class Main {
     }
 
     public static void call() throws Exception {
-        Model model = new Model();
-        model.add(new LabWork("123", new Coordinates(1, 1.2f), 0.1f, Difficulty.EASY, new Discipline("321", 321, (long) 3, 2)));
+        LabWork labWork = new LabWork("123", new Coordinates(1, 1.2f), 0.1f, Difficulty.EASY, new Discipline("321", 321, (long) 3, 2));
         // Coordinates cords = ;
 
         ObjectMapper mapper = new ObjectMapper();
         mapper.findAndRegisterModules();
-    	String jsonBook = mapper.writeValueAsString(model);
+    	String jsonBook = mapper.writeValueAsString(labWork);
     	System.out.println(jsonBook);
+
+        ObjectMapper mapper1 = new ObjectMapper();
+        mapper1.findAndRegisterModules();
+        LabWork labWork1 = mapper1.readValue(jsonBook, LabWork.class);
+        System.out.println(labWork1.toString());
+
     }
 }
