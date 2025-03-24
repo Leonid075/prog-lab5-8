f4e660fdc443e70e273b2317de758479e6f68f1e
Leonid075 <grebenukleonid075@gmail.com>
2025-03-24 09:21:16 +0300

    update Main.java for test

--- BUILD
+++ BUILD
@@ -6,24 +6,31 @@ java_binary(
     name = "main",
     main_class = "Main",
     srcs = glob(["src/main/java/Main.java"]),
-    deps = [":labwork", ":jackson", ":cm"],
+    deps = [":labwork", ":jackson", ":cm", ":jackson-dataformat", ":commands",],
 )
 
 java_library(
     name = "labwork",
     srcs = glob(["src/main/java/ru/p3xi/labwork/*.java"]),
+    deps = [":jackson", ":jackson-annotations"],
 )
 
 java_library(
     name = "cm",
     srcs = glob(["src/main/java/ru/p3xi/cm/*.java"]),
-    deps = [":labwork"],
+    deps = [":labwork", ":jackson", ":jackson-dataformat", ":jackson-annotations",],
+)
+
+java_library(
+    name = "commands",
+    srcs = glob(["src/main/java/ru/p3xi/commands/*.java"]),
+    deps = [":labwork", ":cm", ":jackson", ":jackson-dataformat",],
 )
 
 java_import(
     name = "jackson",
     jars = ["src/main/java/jackson-databind-2.16.1.jar",],
-    deps = [":jackson-core", ":jackson-annotations", ":jackson-datatype"],
+    deps = [":jackson-core", ":jackson-annotations", ":jackson-datatype", "jackson-dataformat"],
 )
 
 java_import(
@@ -39,4 +46,15 @@ java_import(
 java_import(
     name = "jackson-datatype",
     jars = ["src/main/java/jackson-datatype-jsr310-2.16.1.jar",],
+)
+
+java_import(
+    name = "stax2-api",
+    jars = ["src/main/java/stax2-api-4.0.0.jar",],
+)
+
+java_import(
+    name = "jackson-dataformat",
+    jars = ["src/main/java/jackson-dataformat-xml-2.16.1.jar",],
+    deps = [":jackson-annotations", ":jackson-datatype", "jackson-core", "stax2-api"],
 )
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
 import ru.p3xi.cm.Model;
+import ru.p3xi.commands.AddCommand;
+import ru.p3xi.commands.SaveCommand;
 import ru.p3xi.labwork.*;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.Console;
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 
 public class Main {
@@ -11,21 +17,34 @@ public class Main {
         catch (Exception e) {
             System.out.println(e);
         }
+        
     }
 
     public static void call() throws Exception {
-        LabWork labWork = new LabWork(new LabWorkBuilder());
-        // Coordinates cords = ;
+        Console con = System.console();
 
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.findAndRegisterModules();
-    	String jsonBook = mapper.writeValueAsString(labWork);
-    	System.out.println(jsonBook);
+        Model model = Model.load(System.getenv("lab5"));
 
-        ObjectMapper mapper1 = new ObjectMapper();
-        mapper1.findAndRegisterModules();
-        LabWork labWork1 = mapper1.readValue(jsonBook, LabWork.class);
-        System.out.println(labWork1.toString());
+        // XmlMapper mapper1 = new XmlMapper();
+        // mapper1.findAndRegisterModules();
+        // Model model = mapper1.readValue("", Model.class);
+        // System.out.println(labWork1.toString());
+
+        // AddCommand add = new AddCommand();
+        // SaveCommand save = new SaveCommand();
+
+        // add.execute(model, add.fillArgs(con));
+        System.out.println(model.getById(0));
+
+        // add.execute(model, add.fillArgs(con));
+        System.out.println(model.getById(1));
+
+        // save.execute(model, save.fillArgs(con));
+
+        // XmlMapper mapper = new XmlMapper();
+        // mapper.findAndRegisterModules();
+    	// String jsonBook = mapper.writeValueAsString(labWork);
+    	// System.out.println(jsonBook);
 
     }
 }
